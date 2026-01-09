fcd0594d22de27aeda71fb6991e29e604bd58fce
alamin-shanto <mdalaminshanto1993@gmail.com>
2026-01-09 22:32:03 +0600

    all guides route added

--- src/routes/guides.routes.ts
+++ src/routes/guides.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { getGuideStats } from "../controllers/guides.controller";
 import { protect } from "../middleware/auth";
+import { listGuides } from "./../controllers/guides.controller";
 
 const router = Router();
 
+router.get("/", listGuides);
 router.get("/stats/:id", protect, getGuideStats);
 
 export default router;
